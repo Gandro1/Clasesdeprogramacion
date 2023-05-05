6ed2049606bff881234845805566606511f40233
Gabriel Viloria <113041967+Gandro1@users.noreply.github.com>
2023-05-05 15:22:30 -0400

    importando primeras clases de javascript

---
+++ javascrip I/desafios/desafio 1/descafio1.js
@@ -0,0 +1,8 @@
+/*definiendo las variables*/
+var namee = "Gabriel";
+const cedula = 29956952;
+var soltero = true
+/*declarando las variables*/
+console.warn("Nombre " + namee);
+console.warn("cedula " + cedula);
+console.warn("¿Es solteron? " + soltero);
---
+++ javascrip III/js/java.js
@@ -0,0 +1,4 @@
+let titulo = document.createElement('h1');
+titulo.textContent = 'Chao Mundo'
+titulo.onclick
+//this.parentNode.removeChild(this)
